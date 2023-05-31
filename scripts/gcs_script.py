8c958756cd9cf0b676bfcb7cac80ad7040f1a67d
tjsos <tony.jacob@uri.edu>
2023-05-31 14:52:00 -0400

    feat:multithreading

--- scripts/gcs_script.py
+++ scripts/gcs_script.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import driver
 import socket
+import threading
+import time
 
 
 class CommsGCS:
@@ -9,35 +11,63 @@ class CommsGCS:
         self.PORT = PORT  # The port used by the server
         self.gc = driver.GroundControlChannel(device=device, baud=baud)
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.bind((HOST, PORT))
         self.gc.wait_heartbeat()
-        # print("rx hb")
-        self.s.connect((HOST, PORT))
+        print("Heartbeat Received")
 
-    def recv_match(self):
-        return self.gc.recv_match()
+        #One thread to monitor new MAVmsgs, One thread to listen for new clients and send msg through sockets.
+        y = threading.Thread(target=self.listen).start()
+        x = threading.Thread(target=self.recv_mavlink_msg).start()
+        
 
-    def encode_and_send(self, message):
+    def listen(self):
+        print("Starting to listen for socket client")
+        self.s.listen()
+        while True:
+            conn, addr = self.s.accept()
+            print("Got connection from", conn)
+            #Each new connection gets a new thread
+            threading.Thread(target=self.start_socket, args=(conn,)).start()
+            print(f'[Active Connections]: {threading.activeCount() - 1}')
+    
+    def start_socket(self, conn):
+        type_msg = ""
+        while 1:
+            try:
+                if self.msg != None:
+                    type_msg = self.msg.get_type()
+                    print(type_msg)
+                    if type_msg == 'GPS_INPUT':
+                        self.send_gps(self.msg, conn)
+                    elif type_msg == 'ATTITUDE':
+                        self.send_attitude(self.msg, conn)
+                    time.sleep(0.5)
+            except:
+                conn.close()
+    
+    def recv_mavlink_msg(self):
+        print("Receiving MAVmsgs")
+        self.msg = self.gc.recv_match()
+        # print(self.msg)
+        time.sleep(0.5)
+        self.recv_mavlink_msg()
+
+    def encode_and_send(self, message, conn):
         message = message.encode()
         print(message)
-        self.s.sendall(message)
+        conn.sendall(message)
         # print("sent")
 
-    def send_gps(self,msg):
-        message = f"[{msg.lat}" + "," + f"{msg.lon}" + "," + f"{msg.alt}]"
-        self.encode_and_send(message)
+    def send_gps(self,msg, conn):
+        message = f"[GPS, {float(msg.lat)}" + "," + f"{float(msg.lon)}" + "," + f"{float(msg.alt)}]"
+        self.encode_and_send(message, conn)
 
-    def send_attitude(self, msg):
-        message = f"[{msg.roll}" + "," + f"{msg.pitch}" + "," + f"{msg.roll}]"
-        self.encode_and_send(message)
+    def send_attitude(self, msg, conn):
+        message = f"[ATTITUDE, {msg.roll}" + "," + f"{msg.pitch}" + "," + f"{msg.roll}]"
+        self.encode_and_send(message, conn)
 
 
 if __name__ == "__main__":
     comms = CommsGCS(HOST= "192.168.1.186", PORT=8080, device="/dev/ttyUSB1", baud=57600)
-    while 1:
-        msg = comms.recv_match()
-        if msg != None:
-            type_msg = msg.get_type()
-            if type_msg == 'GPS_INPUT':
-                comms.send_gps(msg)
-            elif type_msg == 'ATTITUDE':
-                comms.send_attitude(msg)
+    
+    
