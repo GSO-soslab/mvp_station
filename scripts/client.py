002fa87859465f0ee2c6a5e55c10da484bbec1d9
tjsos <tony.jacob@uri.edu>
2023-06-12 14:03:34 -0400

    feat:logs lat-long and battery

--- scripts/client.py
+++ scripts/client.py
@@ -17,11 +17,13 @@ class Visualise:
         if os.path.exists(self.csv_data):
             os.remove(self.csv_data)
         self.csv_file = open(self.csv_data, 'w')
-        header = ['TIME','X', 'Y','ROLL','PITCH','YAW']
+        header = ['TIME','BATTERY', 'X', 'Y','LAT','LONG','ROLL','PITCH','YAW',]
         dw = csv.DictWriter(self.csv_file, delimiter=',', fieldnames=header)
         dw.writeheader()
         self.csv_file.close()
         self.x,self.y,self.roll, self.pitch, self.yaw = 0,0,0,0,0
+        self.lat, self.long = 0,0
+        self.battery = 0.0
 
     def connect_to_gcs(self):
         data = self.s.recv(1024)
@@ -35,27 +37,38 @@ class Visualise:
                 self.parse_yaw()
             elif self.res[0] == 'BAD_DATA':
                 print("Bad data")
+            elif self.res[0] == 'BATTERY_STATUS':
+                self.parse_battery()
             self.write_to_csv()
 
     def parse_gps(self):
         #['GPS', ' 40999994.0', '71000000.0', '2140.774169921875']
-        lat = float(self.res[1]) /10e5
-        long = float(self.res[2]) /10e5
+        self.lat = float(self.res[1]) /10e5
+        self.long = float(self.res[2]) /10e5
         self.alt = float(self.res[3])
         transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
-        self.x, self.y = transformer.transform(long, lat)
+        self.x, self.y = transformer.transform(self.long, self.lat)
 
     def parse_yaw(self):
         #[ATTITUDE, -0.01279214583337307,-0.03361418843269348,-0.01279214583337307]'
         self.roll = float(self.res[1]) 
         self.pitch = float(self.res[2])
         self.yaw = float(self.res[3])
+    
+    def parse_battery(self):
+        #[BATTERY_STATUS, 94]
+        self.battery = float(self.res[1])
+        print(self.battery)
 
     def write_to_csv(self):
         self.csv_file = open(self.csv_data, 'a')
         self.csv_writer = csv.writer(self.csv_file, delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL)
         self.csv_writer.writerow([datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                  self.x,self.y,self.roll, self.pitch, self.yaw])
+                                  self.battery,
+                                  self.x, self.y,
+                                  self.lat, self.long, 
+                                  self.roll, self.pitch, self.yaw, 
+                                  ])
         self.csv_file.close()
         
 
