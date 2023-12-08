3d976fcbf08ecf66426698fe41a534907258fca7
tjsos <tony.jacob@uri.edu>
2023-12-08 15:39:08 -0500

    fix #2

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import QAction, QInputDialog
-from qgis.core import QgsProject, Qgis
+from qgis.core import QgsProject, Qgis, QgsLayerTreeGroup, QgsFields
+import qgis.core as core
 from qgis.utils import iface
-from qgis.gui import QgsMapToolEmitPoint
+from qgis.gui import QgsMapToolEmitPoint, QgsMapToolPan
 from pyproj import Proj, transform
 from datetime import datetime
 import csv
@@ -14,47 +15,30 @@ class Interface():
     def __init__(self):
         
         self.iface = iface
-        self.iface.messageBar().pushMessage(f"[INFO]", "Click to select Waypoints", level=Qgis.Info)
+        self.canvas = iface.mapCanvas()
+        self.iface.messageBar().pushMessage(f"[INFO]", "Click Add Point to select Waypoints", level=Qgis.Info)
         self.toolbar = self.iface.addToolBar("MVP Station")
-        
-        #creates the button in the panel window
-        self.action = QAction("Send Waypoints / Exit", iface.mainWindow())
-        self.toolbar.addAction(self.action)
-        #on click, runs this function
-        self.action.triggered.connect(self.run_function)
+
+        #Add Waypoints
+        self.undo = QAction("Add Point", iface.mainWindow())
+        self.toolbar.addAction(self.undo)
+        self.undo.triggered.connect(self.add_waypoint)
 
         #Undo button
         self.undo = QAction("Undo Point", iface.mainWindow())
         self.toolbar.addAction(self.undo)
         self.undo.triggered.connect(self.undo_waypoint)
 
-        #Service Button
+        #creates the button in the panel window
+        self.action = QAction("Send Waypoints / Exit", iface.mainWindow())
+        self.toolbar.addAction(self.action)
+        #on click, runs this function
+        self.action.triggered.connect(self.run_function)
+
+        #Terminal Button
         self.service = QAction("Terminal", iface.mainWindow())
         self.toolbar.addAction(self.service)
         self.service.triggered.connect(self.service_callBack)
-
-        self.canvas = iface.mapCanvas()
-        # this QGIS tool emits as QgsPoint after each click on the map canvas
-        self.pointTool = QgsMapToolEmitPoint(self.canvas)
-        self.pointTool.canvasClicked.connect(self.display_point)
-
-        #Textbox to name the group
-        tempTuple = QInputDialog.getText(None, "Mission", "Name your Mission")
-        if tempTuple[1]:
-            group_name = tempTuple[0]
-            if group_name != '':
-                self.group_name = group_name
-            else:
-                self.group_name = "unnamed_mission" + str(random.randint(0,9))
-        else:
-            self.group_name = "unnamed_mission" + str(random.randint(0,9))
-        self.root = QgsProject.instance().layerTreeRoot()
-        self.myGroup = QgsLayerTreeGroup(self.group_name)
-        self.root.insertChildNode(0,self.myGroup)
-        # self.myGroup = self.root.addGroup(self.group_name)
-        
-        #Keeps the cursor on the map
-        self.canvas.setMapTool(self.pointTool)
 
         #list of waypoints.
         self.waypoints = []
@@ -67,7 +51,7 @@ class Interface():
 
     def undo_waypoint(self):
         if self.waypoints == []:
-            self.iface.messageBar().pushMessage(f"[WARN]", f"No more points to remove", level=Qgis.WARNING)
+            self.iface.messageBar().pushMessage(f"[WARN]", f"No more points to remove", level=Qgis.Warning)
         else:
             self.waypoints.pop()
             print(f"[INFO]:Points Selected: {len(self.waypoints)}")
@@ -95,11 +79,11 @@ class Interface():
                                 self.flag])
 
         #Once sent, the script stops running and the cursor is set to the pan tool.
-        self.iface.removeToolBarIcon(self.action)
-        self.iface.removeToolBarIcon(self.undo)
-        self.iface.removeToolBarIcon(self.service)
+        # self.iface.removeToolBarIcon(self.action)
+        # self.iface.removeToolBarIcon(self.undo)
+        # self.iface.removeToolBarIcon(self.service)
 
-        self.iface.mainWindow().removeToolBar(self.toolbar)
+        # self.iface.mainWindow().removeToolBar(self.toolbar)
 
         self.toolPan = QgsMapToolPan(self.canvas)
         self.canvas.setMapTool(self.toolPan)
@@ -135,18 +119,42 @@ class Interface():
 
             #displays the point on the map
             layerFields = QgsFields()
-            writer = QgsVectorFileWriter(fn, 'UTF-8', layerFields, QgsWkbTypes.Point, QgsCoordinateReferenceSystem("EPSG:3857"), "ESRI Shapefile")
-            feat = QgsFeature()
-            feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(cood[0], cood[1])))
+            writer = core.QgsVectorFileWriter(fn, 'UTF-8', layerFields, core.QgsWkbTypes.Point, core.QgsCoordinateReferenceSystem("EPSG:3857"), "ESRI Shapefile")
+            feat = core.QgsFeature()
+            feat.setGeometry(core.QgsGeometry.fromPointXY(core.QgsPointXY(cood[0], cood[1])))
             writer.addFeature(feat)
             del(writer)
 
             #Places in Group
-            layer = QgsVectorLayer(fn, self.point_name, "ogr")
-            self.myGroup.insertChildNode(-1, QgsLayerTreeLayer(layer))
+            layer = core.QgsVectorLayer(fn, self.point_name, "ogr")
+            self.myGroup.insertChildNode(-1, core.QgsLayerTreeLayer(layer))
             QgsProject.instance().addMapLayer(layer, False)
 
             self.waypoints.append([self.lat, self.long, self.alt])
             print(f"[INFO]:Points Selected: {len(self.waypoints)}")
+
+    def add_waypoint(self):
+        # this QGIS tool emits as QgsPoint after each click on the map canvas
+        self.pointTool = QgsMapToolEmitPoint(self.canvas)
+        self.pointTool.canvasClicked.connect(self.display_point)
+
+        #Textbox to name the group
+        tempTuple = QInputDialog.getText(None, "Mission", "Name your Mission")
+        if tempTuple[1]:
+            group_name = tempTuple[0]
+            if group_name != '':
+                self.group_name = group_name
+            else:
+                self.group_name = "unnamed_mission" + str(random.randint(0,9))
+        else:
+            self.group_name = "unnamed_mission" + str(random.randint(0,9))
+        self.root = QgsProject.instance().layerTreeRoot()
+        self.myGroup = QgsLayerTreeGroup(self.group_name)
+        self.root.insertChildNode(0,self.myGroup)
+        # self.myGroup = self.root.addGroup(self.group_name)
+        
+        #Keeps the cursor on the map
+        self.canvas.setMapTool(self.pointTool)
+
             
 l = Interface()
